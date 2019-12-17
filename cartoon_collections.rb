efbb1e94d3cd2e17f77c038047fc850790380d80
Brendan R Borgers <brendanborgers@gmail.com>
2019-12-17 18:16:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,8 +29,9 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
+  
+  cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |cheese|
     cheese_types.include?(cheese)
-  cheese_types = ["cheddar", "gouda", "camembert"]
 end
 end
